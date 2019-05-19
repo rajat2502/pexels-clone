2149bb01fc6e163688f98aa5e541044305f9055e
abc <rajat2502@users.noreply.github.com>
2019-05-19 21:50:02 +0530

    loader add

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     if(title==''){
     alert("Please Enter Some Value Into The Input Box");
  }
- 
+
   $.ajax({
 
     url : "https://pixabay.com/api/?key=12229910-f242d279c47a4d45d1de9b391&q="+title+"&image_type=photo",
@@ -15,20 +15,32 @@ $(document).ready(function(){
     datatype :"jsonp",
     success : function(data){
       console.log(data);
-      console.log(data.hits[0].largeImageURL);
-      $("#1").attr("src",data.hits[0].largeImageURL);
-      $("#2").attr("src",data.hits[1].largeImageURL);
-      $("#3").attr("src",data.hits[3].largeImageURL);
-      $("#4").attr("src",data.hits[4].largeImageURL);
-      $("#5").attr("src",data.hits[5].largeImageURL);
-      $("#6").attr("src",data.hits[6].largeImageURL);
-      $("#7").attr("src",data.hits[7].largeImageURL);
-      $("#8").attr("src",data.hits[8].largeImageURL);
-      $("#9").attr("src",data.hits[9].largeImageURL);
-      $("#10").attr("src",data.hits[10].largeImageURL);
-      $("#11").attr("src",data.hits[11].largeImageURL);
-      $("#12").attr("src",data.hits[12].largeImageURL);
+      console.log(data.hits[0].webformatURL);
+      $("#1").attr("src",data.hits[0].webformatURL);
+      $("#2").attr("src",data.hits[1].webformatURL);
+      $("#3").attr("src",data.hits[3].webformatURL);
+      $("#4").attr("src",data.hits[4].webformatURL);
+      $("#5").attr("src",data.hits[5].webformatURL);
+      $("#6").attr("src",data.hits[6].webformatURL);
+      $("#7").attr("src",data.hits[7].webformatURL);
+      $("#8").attr("src",data.hits[8].webformatURL);
+      $("#9").attr("src",data.hits[9].webformatURL);
+      $("#10").attr("src",data.hits[10].webformatURL);
+      $("#11").attr("src",data.hits[11].webformatURL);
+      $("#12").attr("src",data.hits[12].webformatURL);
     }
   });
   });
+});
+
+$(document).ready(function () {
+    $(document).ajaxStart(function () {
+        $("#loading").show();
+        $(".photos").hide();
+    }).ajaxStop(function () {
+        setTimeout(function(){
+          $("#loading").hide();
+          $(".photos").show();
+        },5000);
+    });
 });
